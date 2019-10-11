09a099151b306978034e29016cdf7c831547cd6d
Lukas Vukovich <lukas.vukovich@gmail.com>
2019-10-11 13:33:18 -0400

    Added spinner loader functionality

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 //Listen for submit
-document
-  .getElementById('loan-form')
-  .addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  document.getElementById('results').style.display = 'none';
+  document.getElementById('loading').style.display = 'block';
+  setTimeout(calculateResults, 1000);
+});
 
 // Calculate Results
-function calculateResults(e) {
-  e.preventDefault();
+function calculateResults() {
   console.log('Calculating...');
   const amount = document.getElementById('amount');
   const interest = document.getElementById('interest');
@@ -23,6 +25,8 @@ function calculateResults(e) {
   const monthly = (principal * x * calculatedInterest) / (x - 1);
 
   if (isFinite(monthly)) {
+    document.getElementById('results').style.display = 'block';
+    document.getElementById('loading').style.display = 'none';
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
@@ -32,6 +36,8 @@ function calculateResults(e) {
 }
 
 function showError(error) {
+  document.getElementById('results').style.display = 'none';
+  document.getElementById('loading').style.display = 'none';
   const errorDiv = document.createElement('div');
   const card = document.querySelector('.card');
   const heading = document.querySelector('.heading');
